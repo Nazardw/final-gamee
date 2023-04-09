371d6c6779809d06a4d0f120f4094c7ce6a90206
Nazardw <128310995+Nazardw@users.noreply.github.com>
2023-04-09 19:05:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -10,15 +10,40 @@ clock = time.Clock()
 btn1 = Button('start_btn.png', 100,100,100, 50)
 btn2 = Button('exit_btn.png', 200,200,200, 50)
 
+bg = image.load("bg.png")
+bg_width = bg.get_width()
+bt_height = bg.get_height()
+tiles = 2
+
+
+run = False
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+        if e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                run = False
+            if e.key == K_LEFT:
+                scroll -= 5
 
 
-    
-    if btn1.draw(window):
-        print("pressed")
+
+
+    if run:
+        for i in range(0, tiles):
+            window.blit(bg, (0,0))
+        scroll -= 5
+
+    else:
+        window.fill((0,0,0))
+        if btn1.draw(window):
+            run = True
+        if btn2.draw(window):
+            game = False
+
+
+
     display.update()
     clock.tick(60)
